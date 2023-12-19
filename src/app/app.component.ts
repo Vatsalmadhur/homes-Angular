595097c24f476f5d5ddb4dafcb9fd9bbeacd7027
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2023-12-19 19:19:49 +0530

    Made a new branch,added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
   // templateUrl: './app.component.html',
   // template: `<h1>Hello world!</h1>`,
   template: `
   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
 `,
